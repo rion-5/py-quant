ccde7ca71c7c8992a00fb6271df13f7f0a6a580b
rion-5 <sklee@hanyang.ac.kr>
2025-01-12 15:27:29 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from data.fetch_data import get_stock_data, get_recent_trading_days
+
+def main():
+  aapl = get_stock_data('AAPL','2025-01-01','2025-01-08')
+  print(aapl)
+
+  [(start_date, end_date)] = get_recent_trading_days()
+  print(start_date)
+  
+
+if __name__ == '__main__':
+  main()
