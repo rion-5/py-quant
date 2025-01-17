54eb927d0da1bbf3dd19bb04b5e8a6c7d64149a0
rion-5 <sklee@hanyang.ac.kr>
2025-01-18 07:12:49 +0900

    edit main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from data.data_fetcher import fetch_stock_data_from_yfinance
+from data.data_fetcher import fetch_stock_data_from_yfinance,fetch_recent_trading_days_from_db
 
 def main():
   aapl = fetch_stock_data_from_yfinance('AAPL','2025-01-01','2025-01-08')
   print(aapl)
 
-  # [(start_date, end_date)] = get_recent_trading_days()
-  # print(start_date)
-  
+  [(start_date, end_date)] = fetch_recent_trading_days_from_db()
+  print(start_date)
+  print(end_date)
 
 if __name__ == '__main__':
   main()
