58ac114816804c4d1a0ae84b4d82e10076f1945f
rion-5 <sklee@hanyang.ac.kr>
2025-07-09 18:55:14 +0900

    add time.sleep at custom_range_data

--- sql/select_group_stock.sql
+++ sql/select_group_stock.sql
@@ -2,3 +2,15 @@ select trading_date,count(*) from stock
 group by trading_date 
 order by trading_date desc
 limit 10;
+
+delete from stock
+where trading_date in
+(
+	with trade_count as (
+	  select trading_date ,count(*) as count from stock
+	  group by trading_date 
+	  order by trading_date desc
+	)
+	select trading_date from trade_count
+	where count < 6600
+);
