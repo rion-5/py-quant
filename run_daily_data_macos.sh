3baf6e70199e80bdea56932ea9057eef2315ac68
rion-5 <sklee@hanyang.ac.kr>
2025-07-16 11:28:56 +0900

    edit mac script

--- run_daily_data_macos.sh
+++ run_daily_data_macos.sh
@@ -4,7 +4,7 @@
 cd /Users/rion5/Zena/py-quant/ || exit
 
 # 로그 디렉토리 확인
-mkdir -p log
+mkdir -p /Users/rion5/Zena/py-quant/log
 
 # 실제 Python 스크립트 실행
-/opt/miniconda3/envs/zena/bin/python -m data.save_daily_data >> log/cron_log_$(date +%Y-%m-%d).log 2>&1
+/opt/miniconda3/envs/zena/bin/python -m data.save_daily_data >> /Users/rion5/Zena/py-quant/log/cron_log_$(date +%Y-%m-%d).log 2>&1
