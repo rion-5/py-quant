ab313bcf575db0075e8abab11db3fa3edc0bb25e
rion-5 <sklee@hanyang.ac.kr>
2025-07-16 11:14:37 +0900

    edit script

--- run_daily_data_ubuntu.sh
+++ run_daily_data_ubuntu.sh
@@ -4,10 +4,8 @@
 cd /home/rion5/Zena/py-quant/ || exit
 
 # 로그 디렉토리 확인
-mkdir -p log
+mkdir -p /home/rion5/Zena/py-quant/log
 
-# 환경 변수 디버깅 로그 (선택사항)
-env > env.log
 
 # 실제 Python 스크립트 실행
 /home/rion5/miniconda3/envs/zena/bin/python -m data.save_daily_data >> /home/rion5/Zena/py-quant/log/cron_log_$(date +%Y-%m-%d).log 2>&1
